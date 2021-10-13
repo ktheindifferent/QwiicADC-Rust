c4af904d65ecf5e1d3d0a9ca5566e83ec262d8fd
Caleb Smith <calebsmithwoolrich@gmail.com>
2021-10-13 16:19:31 -0400

    read_register implimentation

--- src/lib.rs
+++ src/lib.rs
@@ -108,11 +108,11 @@ mod tests {
     fn test_init() {
 
         let config = QwiicADCConfig::default();
-        let mut qwiic_relay = QwiicADC::new(config, "/dev/i2c-1", 0x08).expect("Could not init device");
+        let mut qwiic_relay = QwiicADC::new(config, "/dev/i2c-1", 0x48).expect("Could not init device");
  
         let cfg = qwiic_relay.read_register(0x01).unwrap();
 
-        println!("{}", cfg);
+        println!("cfg: {}", cfg);
 
 
 
